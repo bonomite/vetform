dc4ea8bb625b71fe1e80f6ee45a18c2a0bf3428f
Danny Valle <dannyvalle@me.com>
2023-08-27 02:32:31 -0400

    Add Vue validator modules. Add assets/css with example CSS files.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
-	css: ['primevue/resources/themes/lara-light-blue/theme.css', 'primeflex/primeflex.css', 'primeicons/primeicons.css'],
+	css: ['primevue/resources/themes/lara-light-blue/theme.css', 'primeflex/primeflex.css', 'primeicons/primeicons.css', '@/assets/css/main.scss'],
 	build: {
 		transpile: ['primevue'],
 	},
