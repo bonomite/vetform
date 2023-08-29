2d290d6235e6437f23b5c7d6936cca65f9179d8c
Danny Valle <dannyvalle@me.com>
2023-08-28 22:25:02 -0400

    GTM Event tracking setup.

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,10 @@
 	const gtm = useGtm()
 	function triggerEvent() {
 		gtm.trackEvent({
-			event: 'Test Click',
+			event: 'login',
 			category: 'GTM Test',
 			action: 'click',
-			label: 'Click Me',
+			label: 'Login',
 			value: 5000,
 			noninteraction: false,
 		})
@@ -19,46 +19,11 @@
 	<h1>Hello!</h1>
 	<p>
 		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo, corporis veritatis? Voluptatem impedit assumenda explicabo. Harum est ex voluptas minus ipsa id ullam. Quam numquam,
-		reprehenderit dignissimos corrupti sapiente odio tempora! Quis dolorum, assumenda eligendi nobis veritatis eaque natus adipisci laboriosam eum labore qui quo iste, facilis excepturi ratione
-		eos! At doloribus ipsum velit esse enim. Dolorem quis aspernatur maxime impedit in officiis harum fugit repellat ullam explicabo enim quisquam corporis assumenda laudantium, reprehenderit
-		dolores nobis ea obcaecati laboriosam consectetur laborum ut? Quibusdam commodi doloribus pariatur! Repellat id natus iste ipsa inventore mollitia fugiat odio totam explicabo, saepe blanditiis
-		cupiditate error qui iusto vitae? Sint quisquam voluptatibus dolores temporibus cumque exercitationem, magnam inventore, quis consequuntur eaque autem, minus necessitatibus neque! Ullam facere
-		quaerat maiores vel? Doloremque dignissimos quisquam aspernatur doloribus commodi iste accusamus, consequuntur neque soluta harum ad maxime vel explicabo. Ipsum modi non atque hic temporibus
-		earum commodi? Rerum impedit nulla optio laudantium suscipit ipsa. Corrupti optio dicta explicabo accusantium temporibus exercitationem odio, sequi vel similique aliquam adipisci debitis
-		cupiditate dignissimos, quos eveniet necessitatibus iusto? Perspiciatis, autem. Nemo, possimus ut sed a tenetur perferendis! Eveniet, vero ex adipisci nesciunt dolore veritatis odio beatae
-		voluptatum aut culpa, quos accusamus, perspiciatis amet omnis ducimus exercitationem possimus fugiat eligendi. Amet vero itaque aliquid corrupti architecto? Et error necessitatibus quasi
-		dignissimos. Quae porro enim esse laboriosam, minima atque ex soluta consequatur omnis suscipit sequi laudantium repellat quasi velit perspiciatis aut quaerat saepe eos, voluptas, corporis
-		error. Laborum rem itaque quo blanditiis nemo recusandae qui. Alias aliquam voluptatem explicabo sapiente corrupti consequatur voluptatibus reprehenderit, excepturi sit odio optio, commodi,
-		sunt dolor inventore natus quos non fugiat rem hic. Reiciendis facere eum asperiores sunt placeat quae nostrum corrupti. Ad, repellat tenetur corporis laudantium recusandae dolorum
-		perspiciatis provident, tempore esse consequatur fugit quod neque debitis dolores illum odit iste laboriosam quas corrupti! Eius labore dolorum numquam! Beatae amet harum quas quidem doloribus
-		ad repellat, voluptates facilis dolore eligendi optio obcaecati nam tempore corporis temporibus error reiciendis. Doloremque odio eaque, laboriosam, totam necessitatibus ea mollitia minima
-		natus pariatur laborum cumque autem neque. Illo eligendi qui cum nostrum iusto sint tempora distinctio obcaecati consectetur mollitia cupiditate sed et unde accusamus, tenetur impedit aperiam
-		expedita nam voluptate consequuntur facere non voluptatem. Quo reiciendis nobis, quia expedita tenetur voluptatum magnam magni nam minima itaque. Ad, nam excepturi quos, consequuntur nobis
-		error voluptates dolore provident maxime voluptatibus soluta eligendi recusandae id corporis quas rem quia hic incidunt labore. Delectus ea cumque, libero vel odit aspernatur doloribus
-		praesentium odio veritatis id repellendus quaerat autem dignissimos saepe. Sunt labore cum assumenda totam id earum delectus maiores, esse enim amet pariatur, obcaecati velit quaerat ad non
-		sequi, error dolorem maxime nisi et. Dignissimos sequi blanditiis praesentium vitae qui omnis facere perferendis cupiditate molestias itaque dolore amet, tenetur fugit rerum iure laboriosam,
-		quae maiores officia soluta. Animi, natus consequatur. Obcaecati soluta aspernatur praesentium eum, voluptatem doloremque quam tempore blanditiis iure incidunt. Quos consequuntur, illo labore
-		voluptas porro iste sed veniam quis perspiciatis eius assumenda vel quibusdam commodi accusamus, hic nisi eos praesentium eligendi perferendis culpa magni in error. A, enim voluptate. Dolor
-		cumque placeat qui quasi vitae quibusdam voluptatum non ratione a optio, rem nam consequatur similique cupiditate illum obcaecati ut excepturi! Ullam non odit, placeat veniam harum saepe
-		corrupti consequuntur iure deserunt laborum, id debitis aut nihil officiis officia aliquam, amet fugit. Quos error distinctio voluptatem. Praesentium accusamus sed dolor harum aliquid quos
-		distinctio fugiat commodi accusantium consequatur magni nulla tenetur soluta repellendus, maxime sunt vel unde? Obcaecati itaque laudantium voluptatem vitae, officia neque provident architecto
-		nam animi quaerat possimus perferendis ut ad tenetur numquam atque commodi qui voluptates amet vel. Cumque repellendus fuga culpa optio tempora pariatur a laboriosam quae et! Officia nemo
-		ullam at accusamus voluptate optio vel a enim, earum reprehenderit unde architecto. Delectus consequatur at, vel quo recusandae error qui temporibus mollitia repellat neque porro magnam magni
-		accusamus eligendi reiciendis adipisci officia, officiis velit repellendus? Perspiciatis esse incidunt adipisci expedita est et vel optio vero repudiandae repellat. Ipsam ullam iste eum in
-		saepe! Cum aspernatur placeat, nobis, in commodi magnam laudantium nisi, quia temporibus animi illo veritatis officia odio enim iusto inventore. Repellendus ipsam maxime neque commodi ad
-		facere minima voluptatum esse veritatis temporibus, dolorum nihil? Qui eveniet aperiam veniam necessitatibus nulla, in deleniti, rerum harum ut a quis dicta? Corporis ducimus magnam, illo
-		nemo, odio quaerat harum molestias soluta maiores dolore accusamus. Dolor mollitia laborum voluptatibus suscipit quae sequi itaque quia consequuntur, enim maiores cum asperiores provident sunt
-		repellendus. Officia cum explicabo necessitatibus, expedita eveniet ad ea quas iste obcaecati maiores voluptatibus, quos numquam omnis, est molestias consectetur harum et esse dignissimos
-		ipsam? Dignissimos repellendus consequuntur veritatis! Totam error officiis sed minima laboriosam, illo laudantium deserunt accusantium perferendis culpa optio magni eveniet possimus voluptas
-		tempora alias dolorem assumenda fugiat vel iste pariatur. Perspiciatis officia minima assumenda, rerum nulla soluta hic iste necessitatibus, quisquam tempore eos consectetur voluptatibus
-		voluptatem qui obcaecati iusto laborum quaerat culpa dolorem dicta natus inventore numquam distinctio! Necessitatibus reprehenderit ut ratione molestiae laboriosam quam, culpa optio, adipisci
-		accusamus nemo accusantium nesciunt temporibus qui nihil ex doloremque, maxime alias eligendi ad ipsam! Sit explicabo praesentium sunt vero temporibus cupiditate, quidem accusamus voluptates
-		asperiores totam beatae! Laborum error, repellat unde reiciendis atque maiores dolor ratione quos cumque molestias nulla ut quaerat, officia animi incidunt aspernatur doloremque sint
-		voluptatibus minus optio. Possimus illum nemo architecto laudantium aspernatur hic delectus laborum iure reprehenderit asperiores nisi cum deleniti quae soluta modi facere illo veniam
-		accusamus numquam rem quibusdam, ratione, quas tempore. Non iusto blanditiis reprehenderit placeat sed! Itaque magnam mollitia eaque ad modi. Aspernatur, quis? Rem possimus, corporis expedita
-		odio deleniti ea, illo debitis reprehenderit iusto dolor, hic consequuntur quasi accusamus reiciendis assumenda! Est libero dolorum eius ea sit ut consectetur eveniet, ab amet doloribus, rerum
-		debitis fugiat, a voluptate quis et nisi! Laudantium eligendi reprehenderit obcaecati minima, autem repellendus culpa quibusdam, quod, sequi suscipit aliquam fugiat aspernatur. Molestias
-		asperiores pariatur adipisci vel, saepe tempora unde itaque voluptatem, hic eligendi veniam.
+		reprehenderit dignissimos corrupti sapiente odio tempora! Quis dolorum, assumenda eligendi nobis veritatis eaque natus adipisci laboriosam eum labore qui quo iste, facilis excepturi ratione.
 	</p>
-	<Button label="Click Me" @click="triggerEvent" />
+	<NuxtLink to="/login" @click="triggerEvent">Login</NuxtLink>
+	<hr />
+	<!-- <Button label="Click Me" url="/login" /> -->
+	<hr />
+	<NuxtLink to="https://blender.org">Blender</NuxtLink>
 </template>
--- pages/login.vue
+++ pages/login.vue
@@ -1,3 +1,4 @@
 <template>
 	<h1>Login</h1>
+	<NuxtLink to="/">Back Home</NuxtLink>
 </template>
