2be088c39102839cab5fe028467ef79f2aa6e77f
Thomas Bono <thomasbono.com@gmail.com>
2023-08-29 00:34:48 -0400

    phone number validation, supabase auth sending verification sms

---
+++ components/error.vue
@@ -0,0 +1,22 @@
+<script setup>
+const props = defineProps({
+  errArr: {
+    type: Array,
+    required: true,
+    default: [],
+  },
+})
+</script>
+
+<template>
+  <small v-if="errArr.length > 0" class="p-error error">
+    <span v-for="err of errArr" :key="err.$uid">
+      {{ err.$message }}
+    </span>
+  </small>
+</template>
+
+<style lang="scss" scoped>
+.error {
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,43 +1,82 @@
 <script setup>
-	const gtm = useGtm()
-	function triggerEvent() {
-		gtm.trackEvent({
-			event: 'login',
-			category: 'GTM Test',
-			action: 'click',
-			label: 'Login',
-			value: 5000,
-			noninteraction: false,
-		})
-	}
+import { useVuelidate } from '@vuelidate/core'
+import { email, helpers, minLength, required } from '@vuelidate/validators'
+import { convertToE164 } from '~/utilities/helpers.js'
+//const gtm = useGtm()
+// function triggerEvent() {
+// 	gtm.trackEvent({
+// 		event: 'login',
+// 		category: 'GTM Test',
+// 		action: 'click',
+// 		label: 'Login',
+// 		value: 5000,
+// 		noninteraction: false,
+// 	})
+// }
 
-	function triggerView() {
-		gtm.trackView('Home', '/')
+// function triggerView() {
+// 	gtm.trackView('Home', '/')
+// }
+const client = useSupabaseClient()
+const phoneNumberRegEx = helpers.regex(/^\(\d{3}\) \d{3}-\d{4}$/)
+
+const rules = computed(() => {
+  return {
+    phone: {
+      required: helpers.withMessage(
+        'The phone number field is required',
+        required
+      ),
+      phoneNumberRegEx,
+    },
+  }
+})
+
+const formData = reactive({
+  phone: '',
+})
+
+const v$ = useVuelidate(rules, formData)
+
+async function verify() {
+  v$.value.$validate()
+  //console.log(v$.value)
+  if (!v$.value.$error) {
+    //alert('valid')
+    // GTM event here
+    // supabase auth check here
+    console.log('convertToE164(formData.phone)', convertToE164(formData.phone))
+    let { user, error } = await client.auth.signInWithOtp({
+      phone: convertToE164(formData.phone),
+    })
+
+    console.log('user', user)
+
+    if (error) {
+      console.log('error', error)
+      // message alert to user
+    } else {
+      // if no error route to verify page
+    }
+  } else {
+    //alert('not valid')
+  }
 }
-
-const phone = ref('')
-function verify() {
-alert(phone.value)
-}	
 </script>
 <template>
-	<h1>Hello!</h1>
-	<p>
-		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo, corporis veritatis? Voluptatem impedit assumenda explicabo. Harum est ex voluptas minus ipsa id ullam. Quam numquam,
-		reprehenderit dignissimos corrupti sapiente odio tempora! Quis dolorum, assumenda eligendi nobis veritatis eaque natus adipisci laboriosam eum labore qui quo iste, facilis excepturi ratione.
-	</p>
-	<NuxtLink to="/login" @click="triggerEvent">Login</NuxtLink>
-	<hr />
-	<!-- <Button label="Click Me" url="/login" /> -->
-	<hr />
-	<NuxtLink to="https://blender.org">Blender</NuxtLink>
-	<hr />
-	<div class="flex flex-column">
+  <h1>Welcome to VCA Animal Hospital</h1>
+  <h2>Let's sign in with your phone number</h2>
+
+  <div class="flex flex-column">
     <label for="phone">Phone</label>
-    <InputMask v-model="phone" date="phone" mask="(999) 999-9999" placeholder="(999) 999-9999" />
-    <small id="username-help">Enter your username to reset your password.</small>
+    <InputMask
+      v-model="formData.phone"
+      date="phone"
+      mask="(999) 999-9999"
+      placeholder="(999) 999-9999"
+    />
+    <error :errArr="v$.phone.$errors" />
 
-	<Button label="Continue" @click="verify"/>
-
-</div>
+    <Button label="Continue" @click="verify" />
+  </div>
 </template>
